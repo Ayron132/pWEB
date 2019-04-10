8b527dcbed0074f49c4c8411f6bde758926e8bc3
Ayron132 <48591597+Ayron132@users.noreply.github.com>
2019-04-09 23:50:48 -0300

    Add files via upload

---
+++ fj21-agenda/src/br/com/caelum/agenda/servlet/AdicionaContatoServlet.java
@@ -0,0 +1,62 @@
+package br.com.caelum.agenda.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.ParseException;
+
+import br.com.caelum.agenda.dao.ContatoDao;
+import br.com.caelum.agenda.modelo.Contato;
+
+
+@WebServlet("/AdicionaContatoServlet")
+public class AdicionaContatoServlet extends HttpServlet {
+	protected void service(HttpServletRequest request,HttpServletResponse response)throws IOException, ServletException {
+
+		PrintWriter out = response.getWriter();
+
+        
+        String nome = request.getParameter("nome");
+        String endereco = request.getParameter("endereco");
+        String email = request.getParameter("email");
+        String dataEmTexto = request.getParameter("dataNascimento");
+        Calendar dataNascimento = null;
+
+      
+        try {
+            java.util.Date date = new SimpleDateFormat("dd/MM/yyyy").parse(dataEmTexto);
+            dataNascimento = Calendar.getInstance();
+            dataNascimento.setTime(date);
+        } catch (java.text.ParseException e) {
+            out.println("Erro de conversão da data");
+            return; 
+        }
+
+        
+        Contato contato = new Contato();
+        contato.setNome(nome);
+        contato.setEndereco(endereco);
+        contato.setEmail(email);
+        contato.setDataNascimento(dataNascimento);
+
+    
+        ContatoDao dao = new ContatoDao();
+        dao.adiciona(contato);
+
+       
+        out.println("<html>");
+        out.println("<body>");
+        out.println("Contato " + contato.getNome() +
+                " adicionado com sucesso");        
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
